ba996c016ed4647c74fe93369983d9c5b5d522f3
AndrewBand <guitarist5@i.ua>
2022-07-25 00:38:14 +0300

    to heroku apgrr1

--- src/main/java/CurrencyInfoBot.java
+++ src/main/java/CurrencyInfoBot.java
@@ -34,24 +34,14 @@ public class CurrencyInfoBot extends TelegramLongPollingBot {
         }
         return instance;
     }
-
     @Override
-//    public String getBotUsername() {
-//        return "@CurrencyInfoProjectGroup1Bot";
-//    }
-
     public String getBotUsername() {
-        return "TestKabaBOT";
+        return "AndrNikNameBot";
     }
 
-
     @Override
-//    public String getBotToken() {
-//        return "5416117406:AAE1XHQxbn8TIY2perQrAAiQsNcxlcth9Wo";
-//    }
-
     public String getBotToken() {
-        return "5110494726:AAHvvtZ2yxM8dnzpR730WBz4eeG7haGp9Kw";
+        return "5041766504:AAFn65xA7HvfZ0E_HkNcsBUl-SS8Mym6DwQ";
     }
 
     @Override
